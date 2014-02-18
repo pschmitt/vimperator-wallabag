897b820f1070f90d430602b0cebcd26bb3cd8ce6
Philipp Schmitt <philipp@schmitt.co>
2014-02-18 01:48:57 +0100

    Fix: url is always null

--- wallabag.js
+++ wallabag.js
@@ -2,7 +2,7 @@ let PLUGIN_INFO = xml`
 <VimperatorPlugin>
     <name>wallabag</name>
     <description lang="en">Pocket</description>
-    <version>0.1</version>
+    <version>0.1.1</version>
     <minVersion>3.2</minVersion>
     <author mail="philipp@schmitt.co" homepage="http://lxl.io">Philipp Schmitt</author>
     <updateURL>https://raw.github.com/pschmitt/vimperator-wallabag/master/wallabag.js</updateURL>
@@ -17,7 +17,12 @@ let PLUGIN_INFO = xml`
 
     commands.addUserCommand(['wallabag', 'wg'], 'Wallabag plugin',
         function(args) {
-            let url = args ? args : buffer.URL;
+            let url = buffer.URL;
+
+            if (typeof args != 'undefined' && args.length > 0) {
+                url = args;
+            }
+
             if (wallabag_url == '') {
                 liberator.echoerr('Wallabag URL undefined. Please add let g:wallabag_url="http://YOURURL" to your vimperatorrc');
             } else {
